93d6ff4f7a97cc56264e4014849870cd352ff3fb
Galatée Lardière <galatee.lardiere@gmail.com>
2022-06-29 23:07:32 +0200

    individual card overview

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "@pages/Home";
 import Navigation from "@components/Navigation";
