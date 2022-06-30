e33aa7024f257381aca4f6633752630a84a9d751
alizeehamon <alizee.hamonpro@gmail.com>
2022-06-30 15:03:30 +0200

    petit fix pages de log

--- frontend/src/pages/Intro.jsx
+++ frontend/src/pages/Intro.jsx
@@ -1,9 +1,12 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import imageintro from "../assets/image-intro-desktop.png";
 import logointro from "../assets/logo_apseed.png";
 import "./Intro.css";
 
 function Intro() {
+  const navigate = useNavigate();
   return (
     <div className="intro-container">
       <div>
@@ -14,7 +17,7 @@ function Intro() {
       </div>
       <h1 className="title-intro"> TITLE</h1>
       <p className="explications-intro"> blablabla</p>
-      <button type="button" className="button-getstarted">
+      <button type="button" className="button-getstarted" onClick={()=> navigate("../authentification", { replace: true })}>
         Get Started
       </button>
     </div>
